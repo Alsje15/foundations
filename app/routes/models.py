49e7449c8ffda3c3dac0b6dfcb9a50374f8f2ac3
Alsje <alsje@mernok.co.za>
2022-04-28 14:11:30 +0200

    updated models

--- app/routes/models.py
+++ app/routes/models.py
@@ -3,9 +3,9 @@ from flask_login import UserMixin
 
 class Recipe(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
-    slug = db.Column(db.String(50), unique=True)
-    image = db.Column(db.String(250))
-    title = db.Column(db.String(50), nullable=False)
+    slug = db.Column(db.String(250), unique=True)
+    image = db.Column(db.String())
+    title = db.Column(db.String(250), nullable=False)
     category = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text())
     instructions = db.Column(db.Text(), nullable=False)
@@ -16,9 +16,9 @@ class Recipe(db.Model, CRUDMixin):
 
 class Chef(db.Model, CRUDMixin, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    image = db.Column(db.String(250))
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    image = db.Column(db.String())
     description = db.Column(db.Text())
 
